525e04a019d451b5aa1312f4d4fb76faa328bf97
Thiago <thiroma9@gmail.com>
2026-08-20 15:24:55 -0300

    feat: Codificando o programa para executar o projeto

--- Programa01B/Program.cs
+++ Programa01B/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Programa01B;
+
+Console.WriteLine("******SISTEMA LOJA BAKE");
+Console.WriteLine();
+
+//INSTANCIANDO
+Bikec bike = new Bikec( );
+
+Console.WriteLine("Modelo");
+bike.Modelo = Console.ReadLine ();
+
+Console.WriteLine("Bateria");
+bike.Bateria = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Peso da Bateria");
+bike.PesoCiclista = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Quantidade estimada de recargas");
+bike.RecargaMes = int.Parse(Console.ReadLine());
+
+//
+Console.WriteLine("\n---RELATORIO DE DESEMPENHO");
+Console.WriteLine($"Bake: {bike.Modelo.ToUpper()}:");
+Console.WriteLine($"Autonomia Estimada: {bike.CalcularAutonomia():F1}por carga");
+Console.WriteLine($"Consumo Mensal: {bike.CalcularConsumoMensal():F2}NK");
+Console.WriteLine($"A Bake é Economica: {bike.BikeEconomica()}");
